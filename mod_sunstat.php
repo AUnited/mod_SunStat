774e108b3936886ad166d775d4148f4c1418ae4e
DragonZX <DragonZX@yandex.ru>
2014-06-18 13:02:29 +0400

    Initial commit

---
+++ mod_sunstat.php
@@ -0,0 +1,27 @@
+﻿<?php
+# @version		$version 0.1 Amvis United Company Limited  $
+# @copyright	Copyright (C) 2012 AUnited Co Ltd. All rights reserved.
+# @license		GNU/GPL, see LICENSE.php
+# Updated		18th June 2014
+#
+# Site: http://amvis.ru
+# Email: info@amvis.ru
+# Phone
+#
+# Joomla! is free software. This version may have been modified pursuant
+# to the GNU General Public License, and as distributed it includes or
+# is derivative of works licensed under the GNU General Public License or
+# other free or open source software licenses.
+# See COPYRIGHT.php for copyright notices and details.
+
+#Prefixes
+# mr_ = MailRu
+# ym_ = Yandex
+# ga_ = Google
+# pwk_ = Piwik
+# li_ = LiveInternet
+
+// no direct access
+defined('_JEXEC') or die;
+echo 'Hello world';
+?>
